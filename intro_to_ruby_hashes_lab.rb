50903860fa75a4ce4d60fdd9ed69625c6c289c91
isaac chavez <isaacgc0596@gmail.com>
2020-01-31 01:30:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,9 @@ def monopoly_with_third_tier
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
+    },
+    :names{
+      
     }
   }}
   # When you start writing the implementation for this method, copy the Hash
